bf97769b1a5621c319160e3ae3cc9a7f182607c0
Melshine <11990946+Melshine@users.noreply.github.com>
2025-09-01 17:05:58 +0200

    finish ex8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+    let res = [];
+    for (x of arr) {
+        if (!args.includes(x)) {
+            res.push(x);
+        }
+    }
+    return res;
 };
 
 // Do not edit below this line
