66cd0e48fa797454d271b57c8837fd4e3987edfd
Melshine <11990946+Melshine@users.noreply.github.com>
2025-09-01 17:15:58 +0200

    finish ex9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (!(Number.isInteger(a) && Number.isInteger(b))) return 'ERROR';
+    if (a < 0 || b < 0) return 'ERROR';
+    if (a > b) return sumAll(b, a);
+    sum = 0;
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
