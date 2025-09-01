abfcb7df3a27e228669ddb18e7e588a8c8cb5043
Melshine <11990946+Melshine@users.noreply.github.com>
2025-09-01 17:45:20 +0200

    finish ex11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  temp = (fahrenheit - 32) * 5 / 9;
+  return Math.round(temp * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  temp = celcius * 9 / 5 + 32;
+  return Math.round(temp * 10) / 10;
 };
 
 // Do not edit below this line
